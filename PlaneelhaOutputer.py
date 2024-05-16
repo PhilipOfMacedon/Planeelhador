0a143e6916068523abb3a067c7f4b9c9cf67598e
PhilipOfMacedon <filiperodrigues97@gmail.com>
2024-05-16 05:49:33 -0300

    Geração de planilha em progresso - 40%

--- PlaneelhaOutputer.py
+++ PlaneelhaOutputer.py
@@ -10,11 +10,12 @@ def load_data():
     data2 = None
     data3 = None
     data4 = None
+    data5 = None
     
     with open("./text_presets/formats.json") as dict_file:
         data1 = json.load(dict_file)
     
-    with open("./text_presets/conditions.json", encoding="utf8") as dict_file:
+    with open("./text_presets/statements.json", encoding="utf8") as dict_file:
         data2 = json.load(dict_file)
         
     with open("./text_presets/reps.json", encoding="utf8") as dict_file:
@@ -23,7 +24,10 @@ def load_data():
     with open("./text_presets/signataries.json", encoding="utf8") as dict_file:
         data4 = json.load(dict_file)
         
-    data = {**data1, **data2, **data3, **data4}
+    with open("./text_presets/header.json", encoding="utf8") as dict_file:
+        data5 = json.load(dict_file)
+        
+    data = {**data1, **data2, **data3, **data4, **data5}
     
     pp = pprint.PrettyPrinter(depth=8)
     pp.pprint(data["FORMATS"]["default"])
@@ -71,10 +75,56 @@ class PlaneelhaOutputer:
     
     def write_document_header(self, wb, ws, data):
         formats = data["FORMATS"]
+        header = data["HEADER"]
+        proposal = data["STATEMENTS"][self.empresa]["PROPOSTA"]
         
         ws.insert_image(0, 1, "./images/" + self.empresa + ".png", data["FORMATS"]["logo"])
-        line_A2G2_format = wb.add_format(formats["Arial8Regular"] | formats["bold_text"] | formats["middle_left"])
-        ws.merge_range("A2:G2", "Ao Illm.o Sr. Pregoeiro de", line_A2G2_format)
+        A8RML_text = wb.add_format(formats["Arial8Regular"] | formats["middle_left"])
+        table_TITLE_text = wb.add_format(formats["Arial8Regular"] | \
+            formats["middle_center"] | \
+            formats["bold_text"] | \
+            formats["border_thin"] | \
+            formats["BGColors"][self.empresa])
+        table_DESC_text = wb.add_format(formats["Arial8Regular"] | \
+            formats["top_left"] | \
+            formats["border_thin"] | \
+            formats["BGColors"][self.empresa])
+        table_IMUQ_text = wb.add_format(formats["Arial8Regular"] | \
+            formats["middle_center"] | \
+            formats["decimal"] | \
+            formats["border_thin"] | \
+            formats["BGColors"][self.empresa])
+        table_MONEYA8R_text = wb.add_format(formats["Arial8Regular"] | \
+            formats["middle_center"] | \
+            formats["currency"] | \
+            formats["border_thin"] | \
+            formats["BGColors"][self.empresa])
+        table_MONEYA8B_text = wb.add_format(formats["Arial8Regular"] | \
+            formats["middle_center"] | \
+            formats["bold_text"] | \
+            formats["currency"] | \
+            formats["border_thin"] | \
+            formats["BGColors"][self.empresa])
+        A8RML_text = wb.add_format(formats["Arial8Regular"] | formats["middle_left"])
+        A8RMJ_text = wb.add_format(formats["Arial8Regular"] | formats["middle_just"])
+        A8BML_text = wb.add_format(formats["Arial8Regular"] | formats["bold_text"] | formats["middle_left"])
+        A8BMC_text = wb.add_format(formats["Arial8Regular"] | formats["bold_text"] | formats["middle_center"])
+        A8BUMC_text = wb.add_format(formats["Arial8Regular"] | formats["bold_underline_text"] | formats["middle_center"])
+        
+        
+        ws.set_row_pixels(1, formats["rowHeights"]["default"])
+        ws.merge_range("A2:G2", header["A2"], A8BML_text)
+        
+        ws.set_row_pixels(2, formats["rowHeights"]["orgao"])
+        ws.merge_range("A3:G3", header["A3"].format(self.orgao), A8BMC_text)
+        
+        ws.set_row_pixels(3, formats["rowHeights"]["default"])
+        ws.write("A4", header["A4"], A8BML_text)
+        ws.merge_range("B4:G4", "", A8BMC_text)
+        
+        ws.set_row_pixels(4, formats["rowHeights"]["default"])
+        ws.merge_range("A5:G5", header["A5"].format(proposal), A8BMC_text)
+        
     
     def generate_file(self):
         
